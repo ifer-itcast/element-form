90659467b06b877cffcac27b185476600d7f255a
ifer-itcast <yangkangkang@itcast.cn>
2021-01-03 20:57:27 +0800

    校验规则

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,12 @@
     <!-- 卡片组件 -->
     <el-card class="login-card">
       <!-- 登录表单 -->
-      <el-form style="margin-top: 50px" :model="loginForm">
-        <el-form-item>
-          <el-input
-            v-model="loginForm.mobile"
-            placeholder="请输入手机号"
-          ></el-input>
+      <el-form style="margin-top: 50px" :model="loginForm" :rules="loginRules">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input
-            v-model="loginForm.password"
-            placeholder="请输入密码"
-          ></el-input>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" placeholder="请输入密码"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" style="width: 100%">登录</el-button>
@@ -33,6 +27,17 @@ export default {
       loginForm: {
         mobile: '',
         password: '',
+      },
+      // 校验规则
+      loginRules: {
+        mobile: [
+          { required: true, message: '手机号不能为空', trigger: 'blur' },
+          { pattern: /^1[3-9]\d{9}$/, message: '请输入正确的手机号', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 6, max: 16, message: '密码应为6-16位的长度', trigger: 'blur' },
+        ],
       },
     };
   },
